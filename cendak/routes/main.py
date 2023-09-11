31be0128eac14d13bcecc6e3708181f7ea5f152c
Hendra Manudinata <manoedinata@gmail.com>
2023-09-11 15:55:35 +0700

    [WIP] Add full URL copy

--- cendak/routes/main.py
+++ cendak/routes/main.py
@@ -7,6 +7,7 @@ from flask import flash
 
 from cendak.deta import cendak_links
 from cendak.utils import generate_id
+from cendak.utils import success_flash_msg
 
 routes_main = Blueprint("routes_main", __name__)
 
@@ -27,5 +28,5 @@ def add():
         "value": link,
     }, id)
 
-    flash("Sukses! ID: " + id, "success")
+    flash(success_flash_msg(id, url_for("routes_redirect.redirect_id", id=id, _external=True)), "success")
     return redirect(url_for("routes_main.home"))
--- cendak/utils.py
+++ cendak/utils.py
@@ -3,3 +3,22 @@ import string
 
 def generate_id(length: int = 4):
     return ''.join(random.choices(string.ascii_letters, k=length))
+
+def success_flash_msg(id: str, full_url:str):
+    return f"""
+    <h2>
+        Success!
+    </h2>
+    <p>
+        ID: <strong>{id}</strong>. Link:
+    </p>
+    <div class="input-group mb-3">
+        <input id="full_url" type="text" class="form-control" readonly
+        value="{full_url}">
+        <button class="btn btn-outline-dark" type="button" id="full_url_copy" data-clipboard-target="#full_url">Copy</button>
+    </div>
+
+    <script>
+        new ClipboardJS("#full_url_copy");
+    </script>
+    """
